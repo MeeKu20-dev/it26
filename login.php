c5926f2f21df2ea2311ecd24033edf3f0de73771
ジャックスオール <101043495+AllJacks@users.noreply.github.com>
2023-03-21 17:15:22 +0800

    database connection

--- login.php
+++ login.php
@@ -1,3 +1,31 @@
+<?php
+session_start();
+include("database.php");
+include("functions.php");
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if(!empty($username) && !empty($password) && !is_numeric($username)) {
+		$query = "SELECT * FROM users WHERE username='$username' LIMIT 1";
+		$result = mysqli_query($con, $query);
+		if($result) {
+			if($result && mysqli_num_rows($result) > 0) {
+				$user_data = mysqli_fetch_assoc($result);
+				if($user_data['password'] === $password) {
+					$_SESSION['user_id'] = $user_data['user_id'];
+					header("Location: index.php");
+					die;
+				}
+			}
+		}
+		echo "Invalid Credentials!";
+	} else {
+		echo "Please Enter some Valid Information!";
+	}
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -17,7 +45,7 @@
 			<h1>St. Kerby Hospital </h1>
 		</div>
 		
-		<form>
+		<form method="post">
         	<label for="username">Username:</label>
         	<input type="text" id="username" name="username" required />
         	<label for="password">Password:</label>
