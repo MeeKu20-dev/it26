e1d752f0edbfe7affa4e3d6d783e7a161b5ad621
Meeku <mico20.mjr@gmail.com>
2023-03-23 12:36:51 +0800

    okeh

---
+++ adminMain.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+=======
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+>>>>>>> e218cd92c20d52bc42365778eeab6439f17afe3b
+};
---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+    
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "hospital_tan";
+
+    if(!$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name)) {
+        die("failed to connect");
+    }
+?>
